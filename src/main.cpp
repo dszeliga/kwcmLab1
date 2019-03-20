fccf26e628321400adacbc3f11f8210c59c35345
dszeliga <szeliga_d@o2.pl>
2019-03-20 13:40:20 +0100

    3.3 opcja ON

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,27 @@
-#include "trygonometria.h"
+
 #include "iostream"
 #include "string"
+#include "LaborkaConfig.h"
 int main(){
 
-	std::cout << "sin(90)" << degreemath::sin(90) << std::endl;
+	/*std::cout << "sin(90)" << degreemath::sin(90) << std::endl;
 	std::cout << "cos(90)" << degreemath::cos(90) << std::endl;
 	std::cout << "tg(90)" << degreemath::tg(90) << std::endl;
-	std::cout << "ctg(90)" << degreemath::ctg(90) << std::endl;
+	std::cout << "ctg(90)" << degreemath::ctg(90) << std::endl;*/
 
+#ifdef USE_TRIGONOMETRY_DEGREE
+	#include "trygonometria.h"
+#else
+	#include "cmath"
+#endif
+
+double result;
+#ifdef USE_TRIGONOMETRY_DEGREE
+	result = degreemath:sin(90.0);
+#else
+	result = cos(3.14/4.0);
+#endif
+
+	std::cout << "wynik: " << result << std::endl;
 	return 0;
 }
